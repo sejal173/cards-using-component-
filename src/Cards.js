ed081a0a33dd80ff3fb8244266021c026befe958
sejal173 <sejalkakade123@gmail.com>
2024-01-03 09:42:07 +0530

    added react folder

---
+++ src/Cards.js
@@ -0,0 +1,12 @@
+import "./cards.css";
+
+export default function Cards(){
+    return(
+        <>
+            <div className="cards">
+                    <h1>product Cards</h1>
+                    <h2>RS 300</h2>
+            </div>
+        </>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import Cards from './Cards';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+        <h1>Hello</h1>
+      <Cards/>
+  </>
+)
